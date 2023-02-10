c35fa6560a9b0bb8826ebc12e5966ec3f3f327f0
Alex Trimper <trimper.a@gmail.com>
2023-02-10 11:06:44 -0400

    Add ability for user to have name connected to account in sign up or edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root "pages#home"
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+  root 'pages#home'
   resources :posts
   get 'about', to: 'pages#about'
 end
